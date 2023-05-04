04aff7722763aa0408bc4ab0526929743f2dcb91
rubanyukm <rubm999@gmail.com>
2023-05-04 15:24:36 -0400

    added labels and CSS file

--- js/layout.js
+++ js/layout.js
@@ -1,8 +1,12 @@
 
-function layout(data) {
+function getTruncatedLabel(text) {
+    return text.length <= 10 ? text : text.slice(0, 10) + '...';
+}
 
+function layout(data) {
+    let labelHeight = 20;
     let cellWidth = config.width / config.numColumns;
-    let cellHeight = cellWidth;
+    let cellHeight = cellWidth + labelHeight;
 
     let maxRadius = 0.35 * cellWidth;
 
@@ -15,12 +19,16 @@ function layout(data) {
 
         let column = i % config.numColumns;
         let row = Math.floor(i / config.numColumns);
+
         item.x = column * cellWidth + 0.5 * cellWidth;
         item.y = row * cellHeight + 0.5 * cellHeight;
         item.radius = radiusScale(d.renewable);
+
+        item.labelText = getTruncatedLabel(d.name);
+        item.labelOffset = maxRadius + labelHeight;
+
         return item;
     });
-    
+
     return layoutData;
 }
-    
--- js/update.js
+++ js/update.js
@@ -1,20 +1,31 @@
 
-function update() {
+function updateGroup(d, i) {
+    let g = d3.select(this);
 
+    if(g.selectAll('*').empty()) {
+        g.append('circle');
+
+        g.append('text')
+            .classed('label', true);
+    }
+
+    g.classed('country', true)
+        .attr('transform', 'translate(' + d.x + ',' + d.y + ')');
+
+    g.select('circle')
+        .attr('r', d.radius);
+
+    g.select('.label')
+        .attr('y', d.labelOffset)
+        .text(d.labelText);
+}
+
+function update() {
     let layoutData = layout(data);
 
     d3.select('#chart')
-        .selectAll('circle')
+        .selectAll('g')
         .data(layoutData)
-        .join('circle')
-        .attr('cx', function(d) {
-            return d.x;
-        })
-        .attr('cy', function(d) {
-            return d.y;
-        })
-        .attr('r', function(d) {
-            return d.radius;
-        });
-            
+        .join('g')
+        .each(updateGroup);
 }
